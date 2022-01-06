b8e46a24df3eb141e3017485c9bcd5786a7e99a2
pranavgoel29 <pranavgoel29@gmail.com>
2022-01-07 00:23:20 +0530

    Added piece of code that clear the console screen.

--- example.java
+++ example.java
@@ -1,5 +1,13 @@
 public class example{
-    public static void main(String args[]){
-        System.out.println("This is a simple java prog.");
+    public static void main(String[] args){
+
+       // The following line of code is used to clear the console, it uses the ANSI escape code 
+       // \033 means ESC or the escape characte, 033 with [H, we can move the cursor to a specified position, 033[2J, cleans the whole screen.
+       System.out.print("\033[H\033[2J");
+       
+       // Flushing out the remaining bytes
+       System.out.flush();
+
+       System.out.println("This is a simple java prog.");
     }
-}
+ }
