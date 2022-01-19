be9cc65aeda699ee426331e593c6a1b3f8216e3f
pranavgoel29 <pranavgoel29@gmail.com>
2022-01-19 23:08:32 +0530

    Added more function to work upon sets.

--- prog_7_set_and_list.java
+++ prog_7_set_and_list.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.util.Set;
 import java.util.HashSet;
+import java.util.TreeSet;
 
 public class prog_7_set_and_list {
     public static void main(String[] args){
@@ -8,13 +9,17 @@ public class prog_7_set_and_list {
         System.out.flush();
         
         //Set is unordered collection of unique element.
-        //It skips any repeatation or duplication in the values and it is also unordered.
+        //It skips any repeatation or duplication in the values.
 
-        Set<Integer> t = new HashSet<Integer>();
+        Set<Integer> t = new HashSet<Integer>();  //This is HashSet and in this they are in order but for only positive integer when it comes to signed integer it fails to order it.
+
+        // Set<Integer> t = new TreeSet<Integer>();  //In this they are in ordered fashion.
+
         t.add(1);
         t.add(3);
         t.add(1);
         t.add(5);
+        t.add(-2);
         System.out.println("Before removal: " + t);
 
         //Set works really fast specially when we are looking for elemenets.
@@ -29,10 +34,14 @@ public class prog_7_set_and_list {
         //To get the size of the set.
         int size = t.size();
 
+        //To clear a set or remove every element from the set.
+        t.clear();
+
         System.out.println("Set after removal of 1: " + t);
         System.out.println("3 is present or not in set: " + result);
         System.out.println("Set is empty: " + empty);
         System.out.println("Set size: " + size);
+        System.out.println("After clearing set: " + t);
         
     }
 }
